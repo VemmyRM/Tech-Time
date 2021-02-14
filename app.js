a7e3833e71e1445c607d3a84b8d08e47870fb10f
VemmyRM <kwvembu@gmail.com>
2021-02-13 23:04:45 -0500

    changes to page styling, routing and functionality

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+const { update } = require("lodash");
 
-const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Welcome to TechTime, a blog site where anyone can learn or post about technology! Find a post explaining a tech topic you want to learn about, or create a post to explain a topic you're passionate about."
 const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+  "Welcome to TechTime! The world of tech can be vast and daunting for first-time learners. So much of the content online is aimed towards a more advanced audience, making technological innovations difficult for beginners to understand. This site was created to help complete beginners better understand various tech concepts. Using the Feynman Technique, anyone can make a post explaining a tech topic in a beginner-friendly way. By posting about the lastest tech news, or exciting tech concepts, you can help us spread knowledge while cementing your own knowledge. Adapted from a challenge for www.appbrewery.com.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -27,6 +27,7 @@ mongoose.connect("mongodb://127.0.0.1/blogDB", {
 const postSchema = new mongoose.Schema({
   title: String,
   content: String,
+  likes: Number
 });
 
 const Post = mongoose.model("Post", postSchema);
@@ -56,6 +57,7 @@ app.post("/compose", function (req, res) {
   const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
+    likes: 0
   });
 
   post.save(function (err, post, number) {
@@ -65,12 +67,34 @@ app.post("/compose", function (req, res) {
   });
 });
 
+
+// app.post("/posts/:postName", function(req, res){
+//   const id = req.params.postName;
+//   let likes = 0;
+//   Post.findById(id, function(err, post){
+//     if (!err){
+//       likes = post.likes;
+//     }
+//   });
+//   Post.findByIdAndUpdate({id}, {"likes":likes+1}, function(err, post){
+//     if(!err){
+//       res.render("post", {
+//         id:id,
+//         title: post.title,
+//         content: post.content,
+//         likes: post.likes +1,
+//       });
+//     }
+//   })
+// });
+
 app.get("/posts/:postName", function (req, res) {
   const id = req.params.postName;
 
   Post.findById(id, function (err, post) {
     if (!err) {
       res.render("post", {
+        id:id,
         title: post.title,
         content: post.content,
       });
